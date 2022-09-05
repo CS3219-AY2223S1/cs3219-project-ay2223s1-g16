c890849ca675232a61a996f1f3b6e700a1d15f82
Roy Chan <royleochan@gmail.com>
2022-09-05 22:36:30 +0800

    feat: restructure org

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import LoginPage from "./pages/LoginPage/LoginPage";
+import App from "./App";
 import theme from "~/theme/theme";
-import HomePage from "./pages/HomePage/HomePage";
 
 const queryClient = new QueryClient();
 
@@ -17,12 +15,7 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={<HomePage />} />
-          </Routes>
-        </BrowserRouter>
+        <App />
       </ChakraProvider>
     </QueryClientProvider>
   </React.StrictMode>
