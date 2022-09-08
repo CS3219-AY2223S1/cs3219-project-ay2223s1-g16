e340cc28038db343eb0b054a166826eaa0a79f8e
Roy Chan <royleochan@gmail.com>
2022-09-08 22:28:15 +0800

    refactor: restructure component architecture

--- frontend/src/components/Router.tsx
+++ frontend/src/components/Router.tsx
@@ -2,13 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import LoginPage from "~/pages/LoginPage/LoginPage";
 import HomePage from "~/pages/HomePage/HomePage";
+import Layout from "~/components/Layout";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
+        <Route element={<Layout />}>
+          <Route path="/home" element={<HomePage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
