b4835139bc6bbe362e0b63e1e0c70356b0701fca
Roy Chan <royleochan@gmail.com>
2022-09-13 10:58:42 +0800

    fix: matching svc

--- matching-service/handlers.js
+++ matching-service/handlers.js
@@ -1,56 +1,62 @@
-import { addPendingMatch, Match, removeMatch } from './repository.js'
-import { MATCH_FAIL, MATCH_SUCCESS, MATCH_START } from './events.js'
+import { addPendingMatch, Match, removeMatch } from "./repository.js";
+import { MATCH_FAIL, MATCH_SUCCESS, MATCH_START } from "./events.js";
 
 // TODO: Shift into constants file
-const MATCH_TIMEOUT = 30
+const MATCH_TIMEOUT = 10;
 
 let _io = undefined;
 export function setIo(io) {
-    _io = io
+  _io = io;
 }
 
-export async function newMatchHandler({username, difficulty}) {
-    const socket = this
-    socket.username = username
+export async function newMatchHandler({ username, difficulty }) {
+  let timeout;
+  const socket = this;
+  socket.username = username;
 
-    const pendingMatch = await Match.findOne({ where: { ispending: true, difficulty: difficulty } })
+  const pendingMatch = await Match.findOne({
+    where: { ispending: true, difficulty: difficulty },
+  });
 
-    if (pendingMatch == null) {
-        const m = await addPendingMatch(username, difficulty)
-        socket.join(m.roomid)
-        socket.emit(MATCH_START, { timeout: MATCH_TIMEOUT })
+  if (pendingMatch == null) {
+    const m = await addPendingMatch(username, difficulty);
+    socket.join(m.roomid);
+    socket.emit(MATCH_START, { timeout: MATCH_TIMEOUT });
 
-        // Server-side Timer logic
-        setTimeout(async () => {
-            await m.reload()
-                .catch(()=> undefined) // undefined if not found
-            
-            if (m != undefined && m.ispending) {
-                socket.emit(MATCH_FAIL)
-                removeMatch(username)
-            }
-        }, MATCH_TIMEOUT * 1000)
-    } else {
-        pendingMatch.completePendingMatch(username)
-        socket.join(pendingMatch.roomid)
-        _io.to(pendingMatch.roomid).emit(MATCH_SUCCESS, { roomId: pendingMatch.roomid, usernameOne: pendingMatch.username1, usernameTwo: pendingMatch.username2})
-    }
+    // Server-side Timer logic
+    timeout = setTimeout(async () => {
+      await m.reload().catch(() => undefined); // undefined if not found
+
+      if (m != undefined && m.ispending) {
+        socket.emit(MATCH_FAIL);
+        removeMatch(username);
+      }
+    }, MATCH_TIMEOUT * 1000);
+  } else {
+    clearTimeout(timeout);
+    pendingMatch.completePendingMatch(username);
+    socket.join(pendingMatch.roomid);
+    _io.to(pendingMatch.roomid).emit(MATCH_SUCCESS, {
+      roomId: pendingMatch.roomid,
+      usernameOne: pendingMatch.username1,
+      usernameTwo: pendingMatch.username2,
+    });
+  }
 }
 
 export async function leaveMatchHandler({ roomid }) {
-    await Match.destroy({where:{roomid:roomid}})
-    // TODO: Should i make the sockets leave the room?
-    this.emit(MATCH_FAIL)
-    _io.to(Number(roomid)).emit(MATCH_FAIL)
+  await Match.destroy({ where: { roomid: roomid } });
+  // TODO: Should i make the sockets leave the room?
+  this.emit(MATCH_FAIL);
+  _io.to(Number(roomid)).emit(MATCH_FAIL);
 }
 
 export async function disconnectHandler() {
-    const socket = this
-    if (socket.username == undefined) return
-    const roomids = await removeMatch(socket.username)
-    // TODO: Iterating through all rooms including partial matches, possible optimization.
-    roomids.forEach(rid => {
-        _io.to(rid).emit(MATCH_FAIL)
-    })
+  const socket = this;
+  if (socket.username == undefined) return;
+  const roomids = await removeMatch(socket.username);
+  // TODO: Iterating through all rooms including partial matches, possible optimization.
+  roomids.forEach((rid) => {
+    _io.to(rid).emit(MATCH_FAIL);
+  });
 }
-
