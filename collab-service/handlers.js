39640fb7c9e846fc948e0aaaf26e35184be6981b
pangpuncake <pangpuncake@gmail.com>
2022-10-03 20:09:45 +0800

    Remove unused code

--- collab-service/handlers.js
+++ collab-service/handlers.js
@@ -1,17 +1,1 @@
-import { CODE_JOINED, CODE_LEFT, CODE_UPDATE } from "./events.js";
-
-export const connectionHandler = ({ roomId, username }) => {
-  console.log(`New connection to room ${roomId} from ${username}`);
-  socket.join(roomId);
-  socket.to(roomId).emit(CODE_JOINED, username);
-};
-
 export const disconnectHandler = () => {};
-
-export const codeUpdateHandler = ({ roomId, code }) => {
-  socket.to(roomId).emit(CODE_UPDATE, code);
-};
-
-export const disconnectingHandler = () => {
-  socket.to(socket.rooms).emit(CODE_LEFT, "User has left the room");
-};
